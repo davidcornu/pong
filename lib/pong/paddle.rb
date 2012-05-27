b45d6102f34be1b31bc3f255ba8233b7aebeb5f9
David Cornu <davidjcornu@gmail.com>
2012-05-27 01:31:57 -0400

    Smooth scrolling

--- lib/pong/paddle.rb
+++ lib/pong/paddle.rb
@@ -9,25 +9,51 @@ module Pong
         ['|']
       ]
       super
+      self.x = 1
       self.y = 10
-      @last_direction = :none
+      reset_directions
     end
 
     def move!(frame, direction)
-      if @last_direction != :none && direction == :none
-        current_direction = @last_direction
+      if direction == :none
+        current_direction = last_direction
       else
         current_direction = direction
       end
 
-      case current_direction
-      when :up
-        self.y += 1
-      when :down
-        self.y -= 1
-      end
+      cant_go_up   = (current_direction == :up && self.y == frame.height)
+      cant_go_down = (current_direction == :down && self.y - self.height + 1 == 1)
 
-      @last_direction = direction
+      if cant_go_up || cant_go_down
+        current_direction = :none
+        reset_directions
+      else
+        case current_direction
+        when :up
+          self.y += 1
+        when :down
+          self.y -= 1
+        end
+        track_direction(direction)
+      end
+    end
+
+    private
+
+    def reset_directions
+      @direction_history = [:none, :none, :none]
+    end
+
+    def track_direction(direction)
+      @direction_history.pop
+      @direction_history.unshift(direction)
+    end
+
+    def last_direction
+      @direction_history.each do |direction|
+        return direction if direction != :none
+      end
+      return :none
     end
   end
 end
